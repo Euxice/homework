78a3bd153cb17e6b3a80cd1d9a5b5894550a8e80
Euxice <a15249434686@163.com>
2026-03-15 11:21:18 +0800

    Add files via upload

---
+++ highchu2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+
+int main()
+{
+	std::string A = " ";
+	int B = 0;
+	int n = 0;
+	std::cin >> A >> B >> n;
+	int a[250] = { 0 };
+	int t = 0;
+	for (int i = 0; i < A.size(); i++)
+	{
+		a[i] = A[i] - '0';
+		//v += (a[i] * std::pow(10, i));
+	}
+	int v = std::stoi(A);
+	a[0] = v / B;
+	
+	t = v % B;
+	std::cout << a[0]<<'.';
+	for (int i = 1; i <= n; i++)
+	{
+		a[i] = t * 10 / B;
+		t = t * 10 % B;
+		std::cout << a[i];
+	}
+
+	return 0;
+}

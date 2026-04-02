59f3f4d001e497a21001445a9c24ca4df6a3e67e
Euxice <a15249434686@163.com>
2026-04-02 22:21:27 +0800

    Add files via upload

---
+++ hash（wait).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <queue>
+using namespace std;
+
+unordered_map<int, string> map;
+
+
+int main()
+{
+	int a = 0;
+	std::cin >> a;
+	if (a == 114)
+	{
+		std::cout << "Welcome to the hash world!" << std::endl;
+	}
+
+
+
+
+
+
+
+	return 0;
+}
